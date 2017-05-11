4079aea7cac34292a4d283b13d38a22fb7284bae
selene <selene@10.0.1.5>
2017-05-11 14:10:01 +0200

    qa structure

--- public_html/insert.js
+++ public_html/insert.js
@@ -12,43 +12,68 @@ var documents = JSON.parse(fs.readFileSync('data/reviews.json', 'utf8'));
 
 
 var client = new elasticsearch.Client({
-  host: 'localhost:9200',
-  log: 'trace'
+    host: 'localhost:9200',
+    log: 'trace'
 });
 
 
 client.ping({
-  requestTimeout: 30000,
-}, function (error) {
-  if (error) {
-    console.error('elasticsearch cluster is down!');
-  } else {
-    console.log('All is well');
-  }
+    requestTimeout: 30000,
+}, function(error) {
+    if (error) {
+        console.error('elasticsearch cluster is down!');
+    } else {
+        console.log('All is well');
+    }
 });
 
 //client.indices.delete({index: 'myindex'});
 
+
+var settings = {
+    "analysis": {
+      "filter": {
+        "my_synonym_filter": {
+          "type": "synonym", 
+          "synonyms": [ 
+            "Maria,Madonna,Vergine",
+            "quadro,opera,dipinto"
+          ]
+        }
+      },
+      "analyzer": {
+        "my_synonyms": {
+          "tokenizer": "standard",
+          "filter": [
+            "lowercase",
+            "my_synonym_filter" 
+          ]
+        }
+      }
+    }
+  };
+  
+client.indices.putSettings({
+  index: myindex,
+  body: settings
+});
+
 var id = 0;
-documents.reviews.map(function(doc){
-console.log("title", doc.title);
-doc.qa.map(function (qa){
-    
-    
-    
+documents.reviews.map(function(doc) {
+    console.log("title", doc.title);
+    doc.qa.map(function(qa) {
 
-
-client.create({
-  index: 'myindex',
-  type: 'mytype',
-  id: id++,
-  body: {
-    // put the partial document under the `doc` key
-    doc: {title: doc.title, question: qa.question, answer: qa.answer}
-  }
-}, function (error, response) {
-  // ...
-        console.log("error", error);
-})
-})
+        client.create({
+            index: 'myindex',
+            type: 'mytype',
+            id: id++,
+            body: {
+                // put the partial document under the `doc` key
+                doc: {title: doc.title, question: qa.question, answer: qa.answer}
+            }
+        }, function(error, response) {
+            // ...
+            console.log("error", error);
+        })
+    })
 })
