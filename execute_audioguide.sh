992ee12c6b2570469ac70c23606f70e88586c222
Andrea Devola <andreadevola@pc-devola.crs4.it>
2020-02-06 11:18:47 +0100

    added loading messages

--- execute_audioguide.sh
+++ execute_audioguide.sh
@@ -1,19 +1,14 @@
 #!/bin/bash
 #IF YOU INSTALLED ELASTICSEARCH THROUGHT BREW UNCOMMENT NEXT ROW:
-#brew services start elasticsearch
-#IF YOU INSTALLED ELASTICSEARCH IN ANOTHER DIR UNCOMMENT NEXT ROW AND REPLACE ‘PATH’ WITH THE COMPLETE PATH FOR ELASTICSEARCH
-#/PATH/bin/elasticsearch & 
+#echo loading elasticsearch ; brew services start elasticsearch
+#IF YOU INSTALLED ELASTICSEARCH IN ANOTHER DIR UNCOMMENT NEXT ROW AND REPLACE ‘PATH’ WITH THE COMPLETE PATH FOR ELASTICSEARCH FOLDER
+#echo loading elasticsearch ; /PATH/bin/elasticsearch & 
 #UNCOMMENT NEXT LINE
-cd $(dirname $0) 
+#cd $(dirname $0) ; sleep 15 
 #UNCOMMENT NEXT LINE
-sleep 15 
+#echo loading express server ; node index.js &  
 #UNCOMMENT NEXT LINE
-node index.js &  
-#UNCOMMENT NEXT LINE
-open -a "Google Chrome" --args --kiosk --app=http://localhost:3000 
-#1--autoplay-policy=no-user-gesture-required
-#UNCOMMENT NEXT LINE
-
+#echo opening application on Google Chrome ; open -a "Google Chrome" --args --kiosk --app=http://localhost:3000  --autoplay-policy=no-user-gesture-required
 #IF YOU PREFER, USE THE COMMAND LINE TO MOVE INTO THE DIRECTORY THAT CONTAINS THIS FILE AND TYPE NEXT LINE IN YOUR TERMINAL 
 #chmod +x execute_audioguide.sh
 
