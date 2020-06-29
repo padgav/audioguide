2c50157a024ad0fe6765c21177988a76c6cb7d72
Antonio Pintori <antoniopintori@Antonios-MacBook-Pro.local>
2020-06-29 11:12:12 +0200

    aggiornati file config

--- execute_audioguide.sh
+++ execute_audioguide.sh
@@ -2,13 +2,13 @@
 #IF YOU INSTALLED ELASTICSEARCH THROUGHT BREW UNCOMMENT NEXT ROW:
 #echo loading elasticsearch ; brew services start elasticsearch
 #IF YOU INSTALLED ELASTICSEARCH IN ANOTHER DIR UNCOMMENT NEXT ROW AND REPLACE ‘PATH’ WITH THE COMPLETE PATH FOR ELASTICSEARCH FOLDER
-#echo loading elasticsearch ; /PATH/bin/elasticsearch & 
+echo loading elasticsearch ; /Users/antoniopintori/Development/ES/elasticsearch/bin/elasticsearch & 
 #UNCOMMENT NEXT LINE
-#cd $(dirname $0) ; sleep 15 
+cd $(dirname $0) ; sleep 15 
 #UNCOMMENT NEXT LINE
-#echo loading express server ; node index.js &  
+echo loading express server ; node index.js &  
 #UNCOMMENT NEXT LINE
-#echo opening application on Google Chrome ; open -a "Google Chrome" --args --kiosk --app=http://localhost:3000  --autoplay-policy=no-user-gesture-required
+echo opening application on Google Chrome ; open -a "Google Chrome" --args --kiosk --app=http://localhost:3000  --autoplay-policy=no-user-gesture-required
 #IF YOU PREFER, USE THE COMMAND LINE TO MOVE INTO THE DIRECTORY THAT CONTAINS THIS FILE AND TYPE NEXT LINE IN YOUR TERMINAL 
 #chmod +x execute_audioguide.sh
 
